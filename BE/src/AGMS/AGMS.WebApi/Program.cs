3d4b89d2b7897a5b8d30c0834212d7a19d19ed35
NDManhHuy <huyndmhe186633@fpt.edu.vn>
2026-02-23 01:42:48 +0700

    Revert "Merge branch 'HuyNDMHE186633' of https://github.com/quangnhattt/SP26_G23_Capstone into HuyNDMHE186633"
    This reverts commit 65064a483a1c110177d3bb44e5296a3aee147502, reversing
    changes made to 6db15f552616400e41a955501ebf72c906a343f7.

--- BE/src/AGMS/AGMS.WebApi/Program.cs
+++ BE/src/AGMS/AGMS.WebApi/Program.cs
@@ -3,9 +3,11 @@ using AGMS.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Bật lại kết nối Database (Hãy chắc chắn trong appsettings.json bạn đã đổi Server=. nhé)
 builder.Services.AddInfrastructure(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,10 +21,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Tạm thời tắt HTTPS Redirection để tránh lỗi cảnh báo màu vàng khi chạy HTTP local
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+// LỆNH QUAN TRỌNG NHẤT: Ép buộc tuyệt đối App phải chạy ở cổng 5259.
+// Lệnh này ở cuối cùng sẽ ghi đè lên mọi cài đặt ngầm đang cố chiếm cổng 5000.
+app.Run("http://localhost:5259");
